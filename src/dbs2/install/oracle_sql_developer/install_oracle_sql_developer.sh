055a25f94391ab6452f81bb7ff5e3f750e092e48
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2024-03-04 12:18:09 +0100

    Add Bellsoft JDK with Jfx, add support for SQLDeveloper 23.1.1

--- src/dbs2/install/oracle_sql_developer/install_oracle_sql_developer.sh
+++ src/dbs2/install/oracle_sql_developer/install_oracle_sql_developer.sh
@@ -8,3 +8,5 @@ chown -R mki.mki /opt/sqldeveloper
 cp $INST_DIR/dbs2/install/oracle_sql_developer/sqldeveloper.desktop /usr/share/applications/
 cp /usr/share/applications/sqldeveloper.desktop $HOME/Desktop/
 chmod +x $HOME/Desktop/sqldeveloper.desktop
+
+
