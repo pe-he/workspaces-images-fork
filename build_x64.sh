2a9f1aa8e612747b82666c0d8bc188d026234d82
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2024-03-05 16:21:21 +0100

    Fix build x86

--- build_x64.sh
+++ build_x64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # build image
-docker build -t peterhertkorn/dbs2-workspace:ss24 --platform linux/amd64 -f dockerfile-dbs2-workspace .
+docker build -t peterhertkorn/dbs2-workspace-:ss24 --platform linux/amd64 -f dockerfile-dbs2-workspace-ss24 .
 
 
