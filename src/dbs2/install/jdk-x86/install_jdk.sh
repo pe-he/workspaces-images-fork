26ef88dcaa2534d5e326cba1c0b21caa4b4de7c1
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2024-03-05 16:07:12 +0100

    Add changes for x86 image

---
+++ src/dbs2/install/jdk-x86/install_jdk.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -ex
+
+cd $INST_DIR/dbs2/install/jdk
+wget https://download.bell-sw.com/java/11.0.22+12/bellsoft-jdk11.0.22+12-linux-amd64-full.deb
+dpkg -i bellsoft-jdk11.0.22+12-linux-amd64-full.deb
+
+
